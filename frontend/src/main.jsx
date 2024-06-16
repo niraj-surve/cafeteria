960719887113d5295ee932478b67b706d3a506c7
niraj <dse21123406@git-india.edu.in>
2024-06-16 14:22:09 +0530

    Minor Fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,5 @@
-// main.js
-
 import React from "react";
-import ReactDOM from "react-dom/client"; // Update import statement
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
